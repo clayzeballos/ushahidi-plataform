5acdef6fec85ac367707ad586cbc47c6babdb02d
Robbie Mackay <rm@robbiemackay.com>
2014-01-23 14:54:47 +1300

    Grab .wiki.cfg from root of repo, not releative to CWD

--- docs/scripts/Wiki.py
+++ docs/scripts/Wiki.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-import sys, xmlrpclib, ConfigParser
+import sys, xmlrpclib, ConfigParser, os
 
 """
 Init class to set up connection to confluence wiki
@@ -10,7 +10,7 @@ class Wiki:
 	def __init__(self):
 		config = self.config = ConfigParser.ConfigParser()
 		#config.readfp(open('defaults.cfg'))
-		config.readfp(open('../.wiki.cfg'))
+		config.readfp(open(os.path.dirname(os.path.realpath(__file__)) + '/../../.wiki.cfg'))
 
 		self.server = xmlrpclib.ServerProxy('https://wiki.ushahidi.com/rpc/xmlrpc')
 		self.token = self.server.confluence2.login(config.get('auth', 'username'), config.get('auth', 'password'))
