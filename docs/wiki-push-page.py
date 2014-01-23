e0d8b33de1a429b8d634a77b855c582c3e22d13f
Robbie Mackay <rm@robbiemackay.com>
2014-01-23 14:14:59 +1300

    Replace misaka markdown rendering with render via Github API

--- docs/wiki-push-page.py
+++ docs/wiki-push-page.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python
 from Wiki import *
-from misaka import Markdown, HtmlRenderer
+import urllib2
+import json
 
 wiki = Wiki()
-md = Markdown(HtmlRenderer())
-
 if len(sys.argv) < 4:
    exit("Usage: " + sys.argv[0] + " spacekey pagetitle markdownfile")
 
@@ -17,16 +16,23 @@ f = open(markdownfile)
 md_title = f.readline().replace("# ", "").replace("\n", "")
 md_content = f.read()
 
-# Render markdown to HTML
-new_content = md.render(md_content)
+# Render markdown via github api
+jdata = json.dumps({
+  "text": md_content,
+  "mode": "markdown",
+  "context": "ushahidi/Lamu"
+})
+response = urllib2.urlopen("https://api.github.com/markdown", jdata)
+new_content = response.read()
 
 # Find current wiki page
 page = wiki.server.confluence2.getPage(wiki.token, spacekey, pagetitle)
 if page is None:
    exit("Could not find page " + spacekey + ":" + pagetitle)
 
-# @todo update title too
-# Update content
+# Update title and content
+page['title'] = md_title
 page['content'] = new_content
+
 # Save
 wiki.server.confluence2.storePage(wiki.token, page)
