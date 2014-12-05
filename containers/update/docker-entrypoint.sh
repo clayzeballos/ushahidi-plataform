ee3afe73c86424a02cd74689941caa91741d36de
Robbie Mackay <rm@robbiemackay.com>
2014-12-05 13:54:17 +1300

    /data -> /code

--- containers/update/docker-entrypoint.sh
+++ containers/update/docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-mkdir -p /data/application/config/environments/development/
+mkdir -p /code/application/config/environments/development/
 
 MYSQL_DATABASE=${MYSQL_DATABASE:-"ushahidi"}
 MYSQL_USERNAME=${MYSQL_USERNAME:-"root"}
@@ -9,13 +9,13 @@ MYSQL_PASSWORD=${MYSQL_PASSWORD:-"changethisrootpassword"}
 
 export MYSQL_DATABASE MYSQL_USERNAME MYSQL_PASSWORD
 
-if [ ! -f "/data/application/config/environments/development/database.php" ]; then
-	cp /data/application/config/database.php /data/application/config/environments/development/database.php
+if [ ! -f "/code/application/config/environments/development/database.php" ]; then
+	cp /code/application/config/database.php /code/application/config/environments/development/database.php
 
-	sed -i "s/'hostname'\s*=>\s*'localhost'/'hostname' => 'db'/" /data/application/config/environments/development/database.php && \
-	sed -i "s/'database'\s*=>\s*'database'/'database' => '$MYSQL_DATABASE'/" /data/application/config/environments/development/database.php && \
-	sed -i "s/'username'\s*=>\s*'username'/'username' => '$MYSQL_USERNAME'/" /data/application/config/environments/development/database.php && \
-	sed -i "s/'password'\s*=>\s*'password'/'password' => '$MYSQL_PASSWORD'/" /data/application/config/environments/development/database.php
+	sed -i "s/'hostname'\s*=>\s*'localhost'/'hostname' => 'db'/" /code/application/config/environments/development/database.php && \
+	sed -i "s/'database'\s*=>\s*'database'/'database' => '$MYSQL_DATABASE'/" /code/application/config/environments/development/database.php && \
+	sed -i "s/'username'\s*=>\s*'username'/'username' => '$MYSQL_USERNAME'/" /code/application/config/environments/development/database.php && \
+	sed -i "s/'password'\s*=>\s*'password'/'password' => '$MYSQL_PASSWORD'/" /code/application/config/environments/development/database.php
 fi
 
 exec "$@"
