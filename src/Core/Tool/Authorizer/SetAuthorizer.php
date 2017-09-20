e5cfdc16e6a5a4f8c1f8f8a9d9ede7332eb74892
Romina Suarez <rowasc@gmail.com>
2017-09-19 21:17:29 -0300

    Only admins can make a saved search featured

--- src/Core/Tool/Authorizer/SetAuthorizer.php
+++ src/Core/Tool/Authorizer/SetAuthorizer.php
@@ -65,9 +65,9 @@ class SetAuthorizer implements Authorizer
 			return false;
 		}
 
-		// Then we check if a user has the 'admin' role or if they are the owner of the entity. If they do they're
+		// Then we check if a user has the 'admin' role. If they are, they're
 		// allowed access to everything (all entities and all privileges)
-		if ($this->isUserAdmin($user) || $this->isUserOwner($entity, $user)) {
+		if ($this->isUserAdmin($user)) {
 			return true;
 		}
 
